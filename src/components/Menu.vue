7d7489c7904f223f4fd653a3d3bf04937fcdebc4
Nuria <c.odena.nuria@gmail.com>
2020-03-11 14:49:00 +0100

    first commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+   <h1>{{hi}}</h1>
+  </div>
+</template>
+
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private hi!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Menu hi="this is a test"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Menu from "@/components/Menu.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Menu,
   }
 };
 </script>
