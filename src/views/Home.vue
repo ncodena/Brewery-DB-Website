d198f0b079cda059364ced6034f0db7429edbcd8
Nuria <c.odena.nuria@gmail.com>
2020-03-11 16:27:03 +0100

    cleaning code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,20 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <Menu brandName="Brewery DB" home="Home" categories="Our Beers" random="Discover"/>
+    <!-- <Menu brandName="Brewery DB" home="Home" categories="Our Beers" random="Discover"/> -->
   </div>
 </template>
 
 <script>
+
 // @ is an alias to /src
 
-import Menu from "@/components/Menu.vue";
+// import Menu from "@/components/Menu.vue";
 
 export default {
   name: "Home",
   components: {
-    Menu,
+    
   }
 };
 </script>
