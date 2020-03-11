ca15f5ddc2c67a488ae3c4e023ee287a7593b62c
Nuria <c.odena.nuria@gmail.com>
2020-03-11 16:01:29 +0100

    navbar created and buttons displayed

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,21 @@
 <template>
+
   <div class="hello">
-   <h1>{{hi}}</h1>
+   <!-- <h1>{{hi}}</h1> -->
+   <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">{{brandName}}</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">{{home}}</b-nav-item>
+        <b-nav-item href="#">{{categories}}</b-nav-item>
+        <b-nav-item href="#">{{random}}</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+
   </div>
+
 </template>
 
 
@@ -11,7 +25,11 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private hi!: string;
+  @Prop() private brandName!: string;
+  @Prop() private home!: string;
+  @Prop() private categories!: string;
+  @Prop() private random!: string
+  
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <Menu hi="this is a test"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Menu brandName="Brewery DB" home="Home" categories="Our Beers" random="Discover"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
 import Menu from "@/components/Menu.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
     Menu,
   }
 };
