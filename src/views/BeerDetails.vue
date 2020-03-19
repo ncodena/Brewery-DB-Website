39bd0b63786c93af1ff5997ca5224156f749f9b5
Nuria <c.odena.nuria@gmail.com>
2020-03-19 11:53:36 +0100

    fixing errors

--- src/views/BeerDetails.vue
+++ src/views/BeerDetails.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="beerDetails">
+    <h1>This is the details beer page</h1>
   </div>
 </template>
+
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "BeerDetails",
+  props: {
+    beer: Object
+  }
+  // methods: {
+  //   ...mapActions(["fetchBeers"])
+  // },
+  // computed: mapGetters(["indexBeers"]),
+  // created() {
+  //   this.fetchBeers();
+  // },
+  // components: {
+  //   BeerCard
+  // }
+};
+</script>
+
+<style lang="scss">
+</style>
