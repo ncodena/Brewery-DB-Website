070b9465498fd6792a82a937643013c1099d074a
Nuria <c.odena.nuria@gmail.com>
2020-03-27 13:03:15 +0100

    refactoring code

--- src/components/BeerCard.vue
+++ src/components/BeerCard.vue
@@ -1,5 +1,27 @@
 <template>
-  <b-card
+<section class="intro" :key="beer.id">
+        <div class="container">
+          <img v-if="beer.image_url" :src="beer.image_url" img-alt="image"/>
+          <img v-else src="assets/beer.png" img-alt="no_image"/>
+        </div>
+        <div class="container">
+          <h6>First Brewed: {{ beer.name }}</h6>
+          <p class="info">{{ beer.first_brewed }}</p>
+        </div>
+      
+
+      <div class="back">
+        <div class="text">
+          <b-card-text>
+            {{ beer.tagline }}
+          </b-card-text>
+          <h3>{{ beer.ingredients.yeast }}</h3>
+            <router-link :to="'/beers/' + beer.name" exact><b-button>Details</b-button></router-link>
+        </div>
+      </div>
+  </section>
+
+  <!-- <b-card
     no-body
     :key="beer.id"
     class="overflow-hidden"
@@ -23,19 +45,9 @@
       <b-col md="6">
         <b-card-body :title="beer.name"> </b-card-body>
       </b-col>
-    </b-row>
-    <div class="middle">
-      <div class="text">
-        <b-card-text>
-          {{ beer.tagline }}
-        </b-card-text>
-        <h3>{{ beer.ingredients.yeast }}</h3>
-        <router-link :to="'/beers/' + beer.name" exact
-          ><b-button>Details</b-button></router-link
-        >
-      </div>
-    </div>
-  </b-card>
+    </b-row> -->
+
+  <!-- </b-card> -->
   <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   <!-- <Menu brandName="Brewery DB" home="Home" categories="Our Beers" random="Discover"/> -->
@@ -56,37 +68,57 @@ export default {
 </script>
 
 <style lang="scss">
-.card-img {
+
+.intro {
+  background-color: white;
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 3rem;
+  margin: 20px 20px 20px 20px;
+  min-height: 70vh;
+  max-width: 26%;
+}
+
+img {
   max-width: 80px;
   min-height: 40%;
 }
-.overflow-hidden:hover .no-gutters {
+.intro :hover .face {
   opacity: 0.2;
 }
-.overflow-hidden:hover .middle {
+.intro:hover .middle {
   opacity: 1;
 }
-.overflow-hidden {
-  max-width: 540px;
-  max-height: 407px;
-  min-height: 70vh;
-  margin: 20px 20px 20px 20px;
-}
+// .overflow-hidden {
+//   max-width: 540px;
+//   max-height: 407px;
+//   min-height: 70vh;
+//   margin: 20px 20px 20px 20px;
+// }
 
-.no-gutters {
+.face {
   min-width: 368px;
   max-width: 368px;
   opacity: 1;
   transition: 1.1s ease;
   backface-visibility: hidden;
 }
-.col-md-6 {
+
+.container {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  min-height: 70vh;
+  justify-content: center;
 }
-.middle {
+
+// .col-md-6 {
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   min-height: 70vh;
+// }
+.back {
   transition: 0.5s ease;
   opacity: 0;
   position: absolute;
@@ -101,4 +133,18 @@ export default {
   justify-content: center;
   align-items: center;
 }
+
+// Media Queries
+
+@media (max-width: 700px) {
+
+  .intro {
+    grid-template-columns: 1fr;
+    min-width: 90%;
+    min-height: 83vh;
+  }
+
+}
+
+
 </style>
