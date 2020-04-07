642b2c1dd8fa8d4df515e76cb6b28786a90bd7e4
Nuria <c.odena.nuria@gmail.com>
2020-04-07 13:34:02 +0200

    adding sorting option to yeast view

--- src/views/YeastView.vue
+++ src/views/YeastView.vue
@@ -1,10 +1,19 @@
 <template>
+<div class="mainContainer">
+  <div class="sortContainer">
+      <div class="sortGroup">
+        <h4>Sort by:</h4>
+        <b-button @click="sortBy('name')">Name</b-button>
+        <b-button @click="sortBy('abv')">Alcohol Volume</b-button>
+      </div>
+    </div>
   <div class="beersList">
     <beer-card
       v-for="beer in indexBeersByCategory"
       :beer="beer"
       :key="beer.id"
     />
+  </div>
   </div>
 </template>
 
@@ -18,7 +27,11 @@ export default {
     beer: Object
   },
   methods: {
-    ...mapActions(["fetchBeersCategory"])
+    ...mapActions(["fetchBeersCategory"]),
+    sortBy(prop) {
+      this.indexBeersByCategory.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
+      console.log(this.indexBeers);
+    }
   },
   computed: {
     ...mapGetters(["indexBeersByCategory"])
@@ -35,6 +48,27 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.mainContainer {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  flex-direction: column;
+}
+.sortContainer {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  color: white;
+}
+
+.sortGroup {
+  display: flex;
+  width: 45%;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+}
+
 .beersList {
   display: flex;
   justify-content: center;
@@ -44,4 +78,14 @@ export default {
 h1 {
   color: white;
 }
+
+ @media (max-width: 700px) {
+    .sortGroup {
+      flex-direction: column;
+      height: 30vh;
+      justify-content: space-around;
+      align-items: center;
+      width: 50%;
+    }
+  }
 </style>
