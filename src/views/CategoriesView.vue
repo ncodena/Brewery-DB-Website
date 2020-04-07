e312de0f4d661bfd649ab318b350c6a78602da8f
Nuria <c.odena.nuria@gmail.com>
2020-04-07 11:06:56 +0200

    cleaning code

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,37 +1,50 @@
 <template>
-<div class="viewContainer">
+  <div class="viewContainer">
     <div class="intro">
-    <h2>Our Beers</h2>
-    <p class="text">Ale or Lager?
-Different strains of yeast behave differently, so that it’s possible to divide the world of beer according to the yeast. The sixty or more defined beer styles in the world can all be sorted by their yeast into two broad families: the ale family and the lager family.
-
-Beers in the ale family are produced by yeast strains that operate better at warmer temperatures. Ales are ready to drink in days rather than weeks, and the yeasts produce extra flavors in addition to creating alcohol: fruity, spicy, or earthy flavors are not unusual. Ales are the traditional beers of England and of Belgium.
-
-Beers in the lager family are fermented by yeast strains that operate better at cooler temperatures. These beers need to be conditioned or cellared (“lager” is German for a storage place) for several weeks or more to reach peak drinkability. The lager beers are the traditional beers of Germany, the Czech Republic and central Europe.
-
-The action of yeast can generate a range of interesting beer flavors and aromas as varied as apple, pepper or apricot. Some, such as banana or clove, are the typical flavors of particular beer styles; others, such as butterscotch, may be considered defects.
-
-</p>
-</div>
-  <div class="beerContainer">
-    <!-- <div v-for="beer in indexCategories" :beer="beer" :key="beer.id">
+      <h2>Ale or Lager?</h2>
+      <p class="text">
+        The most important ingredient in brewing was the last one discovered,
+        because yeast is a single-celled organism that is invisible to the naked
+        eye. Still, brewers have long known that some unseen agent turned a
+        sweet liquid into beer. Long ago, the action of yeast was such a
+        blessing, yet so mysterious, that English brewers called it “Godisgood.”
+      </p>
+      <p class="text">
+        Different strains of yeast behave differently, so that it’s possible to
+        divide the world of beer according to the yeast. The sixty or more
+        defined beer styles in the world can all be sorted by their yeast into
+        two broad families: the ale family and the lager family. Ales are ready
+        to drink in days rather than weeks, and the yeasts produce extra flavors
+        in addition to creating alcohol: fruity, spicy, or earthy flavors are
+        not unusual. Beers in the lager family are fermented by yeast strains
+        that operate better at cooler temperatures. These beers need to be
+        conditioned or cellared (“lager” is German for a storage place) for
+        several weeks or more to reach peak drinkability. The action of yeast
+        can generate a range of interesting beer flavors and aromas as varied as
+        apple, pepper or apricot. Some, such as banana or clove, are the typical
+        flavors of particular beer styles; others, such as butterscotch, may be
+        considered defects.
+      </p>
+    </div>
+    <div class="beerContainer">
+      <!-- <div v-for="beer in indexCategories" :beer="beer" :key="beer.id">
       <router-link :to="'/categories/' + beer.ingredients.yeast" exact
         ><p>{{ beer.ingredients.yeast }}</p></router-link
       >
     </div> -->
-    <b-card
-      v-for="beer in indexCategories"
-      :beer="beer"
-      :key="beer.id"
-      bg-variant="light"
-      class="text-center"
-    >
-      <router-link :to="'/categories/' + beer.ingredients.yeast" exact
-        ><b-card-text>{{ beer.ingredients.yeast }}</b-card-text></router-link
+      <b-card
+        v-for="beer in indexCategories"
+        :beer="beer"
+        :key="beer.id"
+        bg-variant="light"
+        class="text-center"
       >
-    </b-card>
+        <router-link :to="'/categories/' + beer.ingredients.yeast" exact
+          ><b-card-text>{{ beer.ingredients.yeast }}</b-card-text></router-link
+        >
+      </b-card>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -62,16 +75,16 @@ export default {
 }
 
 .intro {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: justify;
-    color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: justify;
+  color: white;
 }
 
 .text {
-    margin: 2px 50px 2px 50px;
+  margin: 2px 50px 2px 50px;
 }
 
 .card {
