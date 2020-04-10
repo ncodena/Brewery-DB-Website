5f1f789538efaf72221fea3771df959888df0a53
Nuria <c.odena.nuria@gmail.com>
2020-04-10 13:22:38 +0200

    creating spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="spinner">
+      <img src="@/assets/spinner.gif" alt="spinner" class="spinner">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+}
+</script>
+<style scoped lang="scss">
+
+</style>
